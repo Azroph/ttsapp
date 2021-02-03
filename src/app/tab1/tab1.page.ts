97fc566a601ca699314207a08d79d50cdf9b4ab0
Antoine Falgiglio <falgitoine@gmail.com>
2021-02-03 11:43:59 +0100

    premier commit

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  constructor(private tts: TextToSpeech) {}
+
+  speak(){
+    this.tts.speak('Hello World')
+    .then(() => console.log('Success'))
+    .catch((reason: any) => console.log('TTs ERROR: ', reason));
+  }
+}
